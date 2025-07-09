02b5f09b1ffff566ce446acb3d6857d751a9c61a
K1ngSY <kingsy8126@outlook.com>
2025-07-08 20:25:57 -0700

    Basic changes to the base class

--- ksubthread.cpp
+++ ksubthread.cpp
@@ -2,4 +2,7 @@
 
 KSubthread::KSubthread(QObject *parent)
     : QObject{parent}
-{}
+{
+    connect(this, &KSubthread::start_signal, this, &KSubthread::handle_start_signal);
+    connect(this, &KSubthread::stop_signal, this, &KSubthread::handle_stop_signal);
+}
--- ksubthread.h
+++ ksubthread.h
@@ -8,17 +8,40 @@ class KSubthread : public QObject
     Q_OBJECT
 public:
     explicit KSubthread(QObject *parent = nullptr);
+    // only access to start its assign.
+    // must emit "start_signal".
+    virtual bool start()
+    {
+        emit start_signal();
+        return true;
+    }
+    // only access to stop itself.
+    // must emit "stop_signal".
+    virtual bool stop()
+    {
+        emit stop_signal();
+        return true;
+    }
 
-public slots:
-    virtual void start() = 0;
-    virtual void stop() = 0;
-
+private slots:
+    virtual void handle_start_signal() = 0;
+    virtual void handle_stop_signal() = 0;
 signals:
-    // finished without exception
-    void finished_0();
-    // finished with exception
-    void finished_1(const QString &msg);
+    // start signals
 
+    // start mission signal.
+    void start_signal();
+    // stop mission signal.
+    void stop_signal();
+
+    // finish signals
+
+    // finished without exception.
+    void finished_0();
+    // finished with exception.
+    void finished_1(const QString &msg);\
+
+    // send message to dashboard log area
     void log_message_Debug(const QString &msg);
     void log_message_User(const QString &msg);
 };
