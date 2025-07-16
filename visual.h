cfeac6301c650cd4c92bfd6b04ce1c22dd0f96a7
K1ngSY <kingsy8126@outlook.com>
2025-07-15 19:55:20 -0700

    Removed the final api

--- visual.h
+++ visual.h
@@ -5,24 +5,12 @@
 #include <opencv2/opencv.hpp>
 #include <windows.h>
 
-// Final interface.
-// Pass a game window hwnd in it, then it will take a screenshot of game
-// and ocr two areas, then return the full-size-screenshot and two text.
-bool analyze_game_window(HWND hwnd,
-                       QString &ocrResult1,
-                       QString &ocrResult2,
-                       QImage &pic1,
-                       QImage &pic2,
-                       QImage &fullImage);
-
-// ____________Helpers____________
-
 cv::Mat QImage_to_cvMat(const QImage &inImage);
 QImage CvMat_to_QImage(const cv::Mat &inMat);
 // OCR Parasaurolophus and tribe log in once.
 bool OCR_image(const QImage &input_image, QString &recognized_text);
-bool OCR_area_P(const QImage &input_full_game_window, QString &recognized_text);
-bool OCR_area_T(const QImage &input_full_game_window, QString &recognized_text);
+bool OCR_area_P(const QImage &input_full_game_window, QString &recognized_text, QImage &debug_roi);
+bool OCR_area_T(const QImage &input_full_game_window, QString &recognized_text, QImage &debug_roi);
 // It will call windows API.
 bool print_window(HWND hwnd, QImage &image);
 // Scan game window, looking for "Start Game" button.
